b57463d7e43f9333b61656a046ea8c451c0e9a67
Poojitha Nakkina <Nakkina.Poojitha@esyasoft.com>
2025-10-27 09:54:09 +0530

    LINQ_TASKS

---
+++ Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_tasks
+{
+    internal class Order
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public double Amount { get; set; }
+        public DateTime OrderDate { get; set; } 
+    }
+}
